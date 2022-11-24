e9552551a5b05515fa9a0b6b307200c11b247876
Archita <asa410@sfu.ca>
2022-11-24 03:47:27 -0800

    Implemented a few test functions in JS

---
+++ Functions/function1.js
@@ -0,0 +1,10 @@
+// Function is created
+function addTwoNums(a,b){
+    return a+b;
+}
+
+// Function is called
+console.log(addTwoNums(3,5));
+
+// build an Array
+var train1 = ["what","how","when","where"];
